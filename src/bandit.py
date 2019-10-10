8b08b781a8697e8bdc4bf21583e1e0fb1401081d
Chandrasekar Sureshkumar <chandrusuresh@gmail.com>
2019-10-10 15:59:23 -0400

    Updated with summary of findings and more experiments
    Added summary to each experiment and added experiments for optimistic initial values and unbiased recency weighting

--- src/bandit.py
+++ src/bandit.py
@@ -29,21 +29,56 @@ def simpleBandit(R,steps,eps,init = 0,alpha=0,randomWalkSigma = 0):
             Q[a] += alpha*(Ri - Q[a])
         if randomWalkSigma != 0:
             Q = [Q[s] + Gaussian(0,randomWalkSigma)[0] for s in range(k)]
-        optimalActions += [a]
-    return reward, optimalActions
+        optimalActions += [1+a]
+    return reward, optimalActions, Q
 
 def runSimpleBandit(R,steps,runs,eps,init=0,alpha=0,randomWalkSigma = 0):
     rewards = []
-    optAct = []
-    idx_R_max = np.where(R == np.max(R))[0]
+    optAct  = []
+    avg_value = []
+    idx_R_max = [1 + a for a in np.where(R == np.max(R))[0]]
     for e in range(len(eps)):
         avg_reward = [0 for i in range(steps)]
         percOptAct = [0 for i in range(steps)]
+        value   = [0 for r in R]
         for i in range(runs):
-            reward, optimalActions = simpleBandit(R,steps,eps[e],init,alpha,randomWalkSigma)
+            reward, optimalActions, Q = simpleBandit(R,steps,eps[e],init,alpha,randomWalkSigma)
             avg_reward = [avg_reward[j] + reward[j]/float(runs) for j in range(steps)]
-            percOptAct = [percOptAct[j] + float(optimalActions[j] in idx_R_max)/float(runs) for j in range(steps)]
+            opt_actCount = [float(a in idx_R_max) for a in optimalActions]
+            percOptAct = [percOptAct[j] + opt_actCount[j]/float(runs) for j in range(steps)]
+            value = [value[j] + Q[j]/float(runs) for j in range(len(Q))]
         rewards += [avg_reward]
         optAct += [percOptAct]
+        avg_value += [value]
+    return rewards,optAct,avg_value
 
-    return rewards,optAct
+
+def unBiased_simpleBandit(R,steps,eps,init = 0,alpha=0,randomWalkSigma = 0):
+    k = len(R)
+    Q = []
+    N = []
+    for i in range(k):
+        Q += [init]
+        N += [0]
+    idx_R_max = np.where(R == np.max(R))[0]
+    reward = []
+    optimalActions = []
+    o = [0]
+    beta = []
+    for i in range(steps):
+        o += [o[i] + alpha*(1-o[i])]
+        beta += [alpha/o[i+1]]
+        prob = np.random.uniform(0,1,1)[0]
+        if prob >= eps:
+            idx_Q_max = np.where(Q == np.max(Q))[0]
+            a = int(np.random.uniform(idx_Q_max[0],idx_Q_max[-1]+1,1)[0])
+        else:
+            a = int(np.random.uniform(0,k,1)[0])
+        Ri = Gaussian(R[a],1.0)[0]
+        reward += [Ri]
+        N[a] += 1
+        Q[a] += beta[i]*(Ri - Q[a])
+        if randomWalkSigma != 0:
+            Q = [Q[s] + Gaussian(0,randomWalkSigma)[0] for s in range(k)]
+        optimalActions += [1+a]
+    return reward, optimalActions, Q, o, beta
